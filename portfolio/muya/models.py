64bbc0e2a453e0ca9169125b5be05dbbe0fac3d5
vincentmuya <vincentmuya13@gmail.com>
2018-03-28 08:40:22 +0300

    regestered the db in settings

---
+++ portfolio/muya/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+
+# Create your models here.
+class profile(models.Model):
+    user = models.ForeignKey(User, null = True)
+    title = models.CharField(max_length = 40)
+    image = models.ImageField(upload_to = 'post/')
+    description = HTMLField()
+    github_link = models.CharField(max_length = 40)
+    deployed_link = models.CharField(max_length = 40)
+
+    def __str__(self):
+        return self.title
+
+    def save_post(self):
+            self.save
+
+# method to call objects from the database
+    @classmethod
+    def this_post(cls):
+        project = cls.objects.all()
+        return project
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vincent/Documents/Projects/core/django/portfolio/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
