b43a56c7b53b5566aa8eb5b84cded00dc313a436
vincentmuya <vincentmuya13@gmail.com>
2019-11-21 18:00:23 +0300

    fixed programming error for contact template

--- muya/views.py
+++ muya/views.py
@@ -4,6 +4,8 @@ from .models import Contact
 from .forms import ContactForm
 from django.http import JsonResponse
 from django.core.mail import mail_admins
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 def index(request):
@@ -26,8 +28,8 @@ def contact(request):
             Question_or_Feedback = "Question_or_Feedback: {}".format(form.cleaned_data['Question_or_Feedback'])
             mail_admins(subject, Question_or_Feedback)
             contact =form.save(commit=False)
-            contact.save()
-            return render(request, 'index.html')
+            # contact.save()
+            return HttpResponseRedirect('/')
     else:
         form = ContactForm()
     return render(request, 'contact.html', {'form':form})
