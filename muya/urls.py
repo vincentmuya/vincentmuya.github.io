cb1d75ec42a33cc3c7fef0f5a9b57641447ddbfe
vincentmuya <vincentmuya13@gmail.com>
2023-02-06 12:08:14 +0300

    modified dependencies and upgraded python

--- muya/urls.py
+++ muya/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import re_path as url, include
 from . import views
 
 urlpatterns=[
